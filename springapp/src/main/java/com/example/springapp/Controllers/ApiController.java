ddb21699a3cba913c6b4d393d8a6c797fffc4497
Iamneo <techadmin@iamneo.ai>
2023-08-16 13:48:17 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/Controllers/ApiController.java
+++ springapp/src/main/java/com/example/springapp/Controllers/ApiController.java
@@ -1,3 +1,15 @@
+package com.example.springapp.Controllers;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApiController {
-    
+
+	
+		  public String Welcome()
+          {
+               return "Welcome String Boot!";
+          }
+
+
 }
